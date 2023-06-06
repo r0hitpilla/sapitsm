6017e1e9fd197749ab7145f94e84aa6d0018d49a
rohitpilla <71271283+r0hitpilla@users.noreply.github.com>
2023-06-06 14:51:59 +0530

    Add files via upload

--- datetime.jsx
+++ datetime.jsx
@@ -1,17 +1,34 @@
 import * as React from 'react';
+import dayjs from 'dayjs';
 import { DemoContainer } from '@mui/x-date-pickers/internals/demo';
-import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
-import { DateTimePicker } from '@mui/x-date-pickers/DateTimePicker';
-import { TimePicker } from '@mui/x-date-pickers/TimePicker';
 
-export default function BasicDateTimePicker() {
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { SingleInputDateTimeRangeField } from '@mui/x-date-pickers-pro/SingleInputDateTimeRangeField';
+import { Button } from '@material-ui/core';
+
+export default function DateTimeRangeFieldValue() {
+  const [value, setValue] = React.useState(() => [
+    dayjs('2022-04-17T15:30'),
+    dayjs('2022-04-21T18:30'),
+  ]);
+
   return (
-    <LocalizationProvider dateAdapter={AdapterDayjs}>
-      <DemoContainer components={['DateTimePicker']}>
-        <DateTimePicker label="Scheduler" />
-        <TimePicker label="Choose Interval" />
+    <><LocalizationProvider dateAdapter={AdapterDayjs}>
+      <DemoContainer
+        components={[
+          'SingleInputDateTimeRangeField',
+          'SingleInputDateTimeRangeField',
+        ]}
+      >
+        <SingleInputDateTimeRangeField
+          label="Start Time"
+          defaultValue={[dayjs('2022-04-17T15:30'), dayjs('2022-04-21T18:30')]} />
+        <SingleInputDateTimeRangeField
+          label="End Time"
+          value={value}
+          onChange={(newValue) => setValue(newValue)} />
       </DemoContainer>
-    </LocalizationProvider>
+    </LocalizationProvider><Button variant="contained" color="primary">Submit</Button></>
   );
 }
